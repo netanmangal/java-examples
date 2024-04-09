9b59729b9eadb5c64cacc23c0b6f78d28826cfaf
netanmangal <imnetanmangal@gmail.com>
2024-04-09 22:52:01 +0530

    Add: 2-Variables

---
+++ 2-Variables.java
@@ -0,0 +1,10 @@
+class Variables {
+    public static void main(String args[]) {
+        String name = "Netan Mangal";
+        System.out.println("String : " + name);
+
+        int length = 5;
+        int breadth = 25;
+        System.out.println("Area of rect : int : " + length * breadth);
+    }
+}
